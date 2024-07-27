287704fb54b050511dc283d5c0a12fefd2903858
p3yn <ivanbigfoot@gmail.com>
2024-07-26 20:05:36 -0700

    Aplicación de películas para el curso de react de sergie code

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BuscadorPelis } from './BuscadorPelis'
+import './styles/buscador.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BuscadorPelis />
+  </React.StrictMode>,
+)
